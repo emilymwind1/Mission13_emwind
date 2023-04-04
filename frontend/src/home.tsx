9f6178a61a2ebc558b17ae3cc1cd9a2118b19084
Emily Reyes <emilymwind1@gmail.com>
2023-04-03 19:35:25 -0600

    Home

--- frontend/src/home.tsx
+++ frontend/src/home.tsx
@@ -2,6 +2,8 @@ import JoelHilton from "./JoelHiltonHeadshot (1).jpg";
 function Home() {
   return (
     <div>
+      <h1>This is the Joel Hilton movie collection.</h1>
+      <p>Continue browsing for more content</p>
       <img src={JoelHilton} alt="Joel" />
     </div>
   );
